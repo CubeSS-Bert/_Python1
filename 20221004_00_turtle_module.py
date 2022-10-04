67c414916e5ff51be46f4b6c1445f87c0747814c
CubeSS-Bert <cfg.cubess@gmail.com>
2022-10-04 22:13:32 +0200

    lists

--- 20221004_00_turtle_module.py
+++ 20221004_00_turtle_module.py
@@ -10,8 +10,13 @@ teken_veelhoek(6, 100, "CW")
 teken_parallellogram("X",100, "CCW", "Red")
 
 
+def print_woord(woord, naam="jef"):
+    print(woord)
+    print(naam)
 
-
+print_woord("hallo")
+print_woord("hallo", 1)
+print_woord("hallo", "bert")
 
 
 
