5fa34597332b5ab81dea30169c0a8ec8bfa6a99f
CubeSS-Bert <cfg.cubess@gmail.com>
2022-10-11 22:15:47 +0200

    Oefeningen 3

--- 10_Oefeningen-basics-01/20221011_00_Oefeningen-basics.py
+++ 10_Oefeningen-basics-01/20221011_00_Oefeningen-basics.py
@@ -13,7 +13,72 @@
 #     exit
 
 # ************************************************ 01.4
-leeftijd = int(input("Geef uw leeftijd: "))
-if leeftijd > 18:
-    print("ok")
-    exit
+# import random as random
+
+# getal = int(random.random()*100)
+# print(getal)
+
+# if getal%2==0:
+#     print(f"Getal {getal} is even")
+# else: 
+#     print(f"Getal {getal} is oneven")
+    
+# # ************************************************ 01.5
+# PENSIOEN_LEEFTIJD = int(67)
+# naam = str(input("Geef uw naam: "))
+# leeftijd = int(input("Wat is uw leeftijd? "))
+# aantal_jaren_tot_pensioen = PENSIOEN_LEEFTIJD - leeftijd
+# print(f"{naam}, binnen {aantal_jaren_tot_pensioen} jaar kan u op pensioen.")
+
+# ************************************************ 01.6
+# getal = int(input("Geef een getal tussen 1 en 100: "))
+
+# if getal%2==0:
+#     print(f"Getal {getal} is even")
+# else: 
+#     print(f"Getal {getal} is oneven")
+
+# ************************************************ 01.7
+# quotering = "F"
+# getal = int(input("Geef een getal  van 0 tot 20:"))
+
+# if getal < 5:
+#     quotering = "F"
+# elif getal < 9:
+#     quotering = "E"
+# elif getal < 12:
+#     quotering = "D"
+# elif getal < 15:
+#     quotering = "C"
+# elif getal < 18:
+#     quotering = "B"
+# else: quotering = "A"
+
+# print(quotering)
+
+# ************************************************ 01.8
+# getal = int(input("Geef een getal tussen 1 en 10:"))
+# for n in range(getal):
+#     if n != 0:
+#         print(n)
+
+# ************************************************ 01.9
+# getal = int(input("Geef een getal tussen 1 en 60:"))
+
+# print("Alle getallen die deelbaar zijn door 9:")
+# for n in range(getal):
+#     if (n%9)==0:
+#         print(n)
+
+# ************************************************ 01.10
+numbers = (range(10))
+reeks = list(input("Geef een reeks van cijfers en tekens."))
+aantal_tekens = len(reeks)
+print(aantal_tekens)
+
+for n in range(aantal_tekens):
+    for m in range(numbers):
+        if reeks[n] == str(numbers[m]):
+            print(reeks[n])
+
+
