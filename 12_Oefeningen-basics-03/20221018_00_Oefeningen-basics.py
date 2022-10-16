7a425a024c8748c3417912b2c01e9a144df125ed
CubeSS-Bert <cfg.cubess@gmail.com>
2022-10-16 16:01:34 +0200

    10_Oefeningen

--- 12_Oefeningen-basics-03/20221018_00_Oefeningen-basics.py
+++ 12_Oefeningen-basics-03/20221018_00_Oefeningen-basics.py
@@ -1,19 +1,41 @@
-class Dobbelsteen:
-    '''
-    Class 'Point'
-    * attributes:
-        - kleur
-        - 
-    * Methods:
-        - gooien
-    '''
+# ************************************************ 03.01
+# leeftijd = 100
+# lijst_bezoekers =[]
+# teller_korting = 0
+
+# print("Geef leeftijd in bezoekers.")
+# while leeftijd > 0:
+#     leeftijd = int(input("/"))
+#     lijst_bezoekers.append(leeftijd)
+
+# print(lijst_bezoekers)    
+# for leeftijd in lijst_bezoekers:
+#     if leeftijd == 0:
+#         exit
+#     elif leeftijd < 7 or leeftijd > 80:
+#         teller_korting = teller_korting + 1
     
-    def __init__(self, kleur="Red"):
-        kleur = str()
-        
-    def __str__(self):
-        return f"Dobbelsteen met kleur {self.kleur}.="
+# print(f"Aantal bezoekers met korting > {teller_korting} van de {len(lijst_bezoekers) - 1}.")
+
+# ************************************************ 03.02
+# temperatuur = 100
+# lijst_temperaturen =[]
+# lijst_negatieve_temperaturen = []
+
+# print("Geef temperaturen.")
+# while temperatuur != 777:
+#     temperatuur = int(input("/"))
+#     lijst_temperaturen.append(temperatuur)
+
+# for temperatuur in lijst_temperaturen:
+#     if temperatuur == 777:
+#         exit
+#     elif temperatuur <= 0:
+#         lijst_negatieve_temperaturen.append(temperatuur)
     
-    def gooien(self):
-        self.resultaat = 1
-        
+# print(lijst_temperaturen)
+# print(lijst_negatieve_temperaturen)
+
+# ************************************************ 03.03
+
+
