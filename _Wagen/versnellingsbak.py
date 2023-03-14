8817dacc50c85f7307567f721144dc405fdf5292
CubeSS-Bert <cfg.cubess@gmail.com>
2023-03-14 22:10:55 +0100

    gg

--- _Wagen/versnellingsbak.py
+++ _Wagen/versnellingsbak.py
@@ -6,4 +6,15 @@ class Versnellingsbak:
     
     def __init__(self):
         self.versnelling = 0
-        self.type = False 
+        self.type = False #False=Automatisch, True=Manueel
+        
+    
+    def naar_boven(self):
+        pass
+    
+    
+    def naar_beneden(self):
+        pass
+    
+    
+    
